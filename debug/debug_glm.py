4abca8d070094c821bf1dce4bae5fb74ba1f7829
vision1v1 <vision1v1@163.com>
2024-01-04 18:31:19 +0800

    调试

--- debug/debug_glm.py
+++ debug/debug_glm.py
@@ -36,6 +36,7 @@ def debug_model_forward():
     # 构造模型
     config = GLMConfig.from_pretrained(glm2b_path)
     model = GLMForConditionalGeneration.from_pretrained(glm2b_path, config=config) # 模型默认加载到cpu上了。
+    model.train()
     
     # 模型输入
     tokenizer:GLMGPT2Tokenizer = GLMGPT2Tokenizer.from_pretrained(glm2b_path)
@@ -43,8 +44,8 @@ def debug_model_forward():
     inputs = tokenizer.build_inputs_for_generation(inputs, targets=["Beijing", "No"], max_gen_length=8, padding=False)
 
     # 移动到相同的设备上。
-    inputs = inputs.to('cuda') # 移动到gpu上。
-    model = model.cuda() # 移动到gpu上。
+    # inputs = inputs.to('cuda') # 移动到gpu上。
+    # model = model.half().cuda() # 移动到gpu上。可以使用半精度进一步降低显存使用，注意了：half() 在cpu上不支持。
 
     # 一次前向传播
     outputs = model.forward(**inputs)
@@ -53,8 +54,29 @@ def debug_model_forward():
     print("loss = ", loss, "logits = ", logits, sep='\n', end='\n\n')
 
 
+def debug_model_inferance():
+    """
+    """
+    config = GLMConfig.from_pretrained(glm2b_path)
+    model = GLMForConditionalGeneration.from_pretrained(glm2b_path, config=config)
+    model.eval()
+
+    tokenizer:GLMGPT2Tokenizer = GLMGPT2Tokenizer.from_pretrained(glm2b_path)
+    inputs = tokenizer(test_data, return_tensors="pt")
+    inputs = tokenizer.build_inputs_for_generation(inputs, max_gen_length=512)
+    
+    # 移动到相同的设备上。
+    # inputs = inputs.to('cuda') # 移动到gpu上。
+    # model = model.half().cuda() # 移动到gpu上。可以使用半精度进一步降低显存使用，注意了：half() 在cpu上不支持。
+    
+    # 模型推理
+    outputs = model.generate(**inputs, max_length=512, eos_token_id=tokenizer.eop_token_id)
+    print(tokenizer.decode(outputs[0].tolist()))
+
+
 if __name__ == "__main__":
     # debug_tokenizer()
     # debug_config()
-    debug_model_forward()
+    # debug_model_forward()
+    debug_model_inferance()
     ...
